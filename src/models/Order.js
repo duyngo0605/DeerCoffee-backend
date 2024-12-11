77ce48058eb321d99b63b6b6e4e194850f2a00c2
duyngo0605 <duyngo06052003@gmail.com>
2024-12-11 09:50:00 +0700

    Update Order.js

--- src/models/Order.js
+++ src/models/Order.js
@@ -2,11 +2,23 @@ const mongoose = require('mongoose');
 
 const orderSchema = new mongoose.Schema({
   table: {type: Number},
-  items: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'MenuItem',
-    required: true,
-  }],
+  items: [
+    {
+      item: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'MenuItem',
+      required: true,
+      },
+      name: {
+        type: String,
+        required: true,
+      },
+      quantity: {
+        type: Number,
+        required: true,
+      },
+    },
+  ],
   totalAmount: {
     type: Number,
     required: true,
