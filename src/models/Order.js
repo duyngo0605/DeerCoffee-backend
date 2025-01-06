befbb7343798067ca21d75cd194fee285ff16161
duyngo0605 <95336529+duyngo0605@users.noreply.github.com>
2025-01-06 11:52:37 +0700

    modify order model

--- src/models/Order.js
+++ src/models/Order.js
@@ -13,6 +13,10 @@ const orderSchema = new mongoose.Schema({
         type: String,
         required: true,
       },
+      price: {
+        type: Number,
+        required: true,
+      },
       quantity: {
         type: Number,
         required: true,
